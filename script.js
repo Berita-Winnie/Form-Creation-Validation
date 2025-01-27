2e5fa8560ce422a1597c939ce7f9d3ddb14894be
Berita-Winnie <beritawinnie@gmail.com>
2025-01-27 15:58:47 +0300

    Removed arrow function and added function instead

---
+++ script.js
@@ -0,0 +1,48 @@
+//Setup and Initial Code Structure
+document.addEventListener('DOMContentLoaded', function() {
+ 
+const form =document.getElementById("registration-form");
+const feedbackDiv =document.getElementById("form-feedback");
+
+//Form Submission and Event Prevention
+ form.addEventListener('submit',event => {
+    event.preventDefault(); //prevent form submission
+
+//input Retreval and Logic    
+const username = document.getElementById('username').value.trim();
+const email = document.getElementById('email').value.trim();
+const password = document.getElementById('password').value.trim();
+
+//Validation Logic
+
+let isValid = true;
+let messages =[];
+   
+if (username.length < 3){
+    isValid = false;
+    messages.push("Username must have at least 3 characters long!");
+}
+
+if (email.includes("@") && email.includes(".")){
+    isValid = false;
+    messages.push("Invalid Email format!");
+}
+
+if (password.length <8){
+    isValid =false;
+    messages.push("Your password must be at least 8 characters long!");
+}
+
+
+//Display Feedback
+feedbackDiv.style.display = "block";
+if(isValid){
+    feedbackDiv.textContent ="Registration Successfull!";
+    feedbackDiv.style.color = "#dc3545";
+} else {
+    feedbackDiv.innerHTML = messages.join("<br>");
+    feedbackDiv.style.color = "#dc3545";
+}
+
+ });
+});
